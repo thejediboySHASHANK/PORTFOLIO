01bf8d002954baa018ec9b9038423c81364dc90a
thejediboySHASHANK <21052358@kiit.ac.in>
2023-05-11 23:00:50 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
+import {styles} from "../styles.js";
+import {navLinks} from "../constants/index.js";
+import {logo, menu, close} from '../assets'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
